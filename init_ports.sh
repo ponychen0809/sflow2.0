2b146b40412cde1ea1a76b7250e7e1c0af385e51
pony <ponychen.cs13@nycu.edu.tw>
2025-11-19 11:46:11 +0800

    add init port.sh

--- init_ports.sh
+++ init_ports.sh
@@ -15,19 +15,18 @@ echo "Using SDE = $SDE"
 ucli
 pm
 
-# 開啟 port 140
+
 port-add 140 40G NONE
 port-enb 140
 
-# 開啟 port 141
+
 port-add 141 40G NONE
 port-enb 141
 
-# 開啟 port 142
+
 port-add 142 40G NONE
 port-enb 142
 
-# 開啟 port 143
 port-add 143 40G NONE
 port-enb 143
 
