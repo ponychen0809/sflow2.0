97d9442b796d94be510401f2486718b231226887
pony <ponychen.cs13@nycu.edu.tw>
2025-12-08 14:53:38 +0800

    update

--- simple_switch_test.py
+++ simple_switch_test.py
@@ -31,24 +31,24 @@ class SimpleSwitchTest(BfRuntimeTest):
 
     def runTest(self):
         # ===== 寫入 4 條轉送規則 =====
-        k1 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 140)])
-        d1 = self.ing_tbl.make_data([gc.DataTuple("port", 141)], "MyIngress.set_out_port")
+        # k1 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 140)])
+        # d1 = self.ing_tbl.make_data([gc.DataTuple("port", 141)], "MyIngress.set_out_port")
 
-        k2 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 141)])
-        d2 = self.ing_tbl.make_data([gc.DataTuple("port", 140)], "MyIngress.set_out_port")
+        # k2 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 141)])
+        # d2 = self.ing_tbl.make_data([gc.DataTuple("port", 140)], "MyIngress.set_out_port")
 
-        k3 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 142)])
-        d3 = self.ing_tbl.make_data([gc.DataTuple("port", 143)], "MyIngress.set_out_port")
+        # k3 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 142)])
+        # d3 = self.ing_tbl.make_data([gc.DataTuple("port", 143)], "MyIngress.set_out_port")
 
-        k4 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 143)])
-        d4 = self.ing_tbl.make_data([gc.DataTuple("port", 142)], "MyIngress.set_out_port")
+        # k4 = self.ing_tbl.make_key([gc.KeyTuple("ig_intr_md.ingress_port", 143)])
+        # d4 = self.ing_tbl.make_data([gc.DataTuple("port", 142)], "MyIngress.set_out_port")
 
-        self.ing_tbl.entry_add(
-            self.dev_tgt,
-            [k1, k2, k3, k4],
-            [d1, d2, d3, d4]
-        )
-        print("✅ ingress_port_forward 規則已寫入完成!!!!")
+        # self.ing_tbl.entry_add(
+        #     self.dev_tgt,
+        #     [k1, k2, k3, k4],
+        #     [d1, d2, d3, d4]
+        # )
+        # print("✅ ingress_port_forward 規則已寫入完成!!!!")
 
         # ===== 啟動背景 thread 每秒送一包到 port 140 =====
         t = threading.Thread(target=self.send_pkt_every_second, daemon=True)
